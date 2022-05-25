af9a3d84775c324a815016120ad93cdda8ba3e24
logico-philosophical <ossia@korea.ac.kr>
2022-05-25 23:13:29 +0900

    Use `yamd` instead

--- docs/documentation-generator.config.js
+++ docs/documentation-generator.config.js
@@ -12,40 +12,40 @@ function getVersion(name) {
 	return require(path.join(dir, 'package')).version;
 }
 
-var m42kup = require('m42kup'),
+var yamd = require('yamd'),
 	hljs = require('highlight.js'),
 	katex = require('katex');
 
-m42kup.set({hljs, katex});
+yamd.set({hljs, katex});
 
 var styles = `<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/highlight.js@${getVersion('highlight.js')}/styles/tomorrow.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@${getVersion('katex')}/dist/katex.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/m42kup@${getVersion('m42kup')}/web/m42kup.default.css">`;
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/yamd@${getVersion('yamd')}/web/yamd.default.css">`;
 
 module.exports = {
 	name: 'documentation-generator 설명서',
 	src: 'src',
 	dst: 'build',
-	render: text => m42kup.render(text),
+	render: text => yamd.render(text),
 	templateData: {
 		styles
 	},
 	list: [
 		{
 			name: '소개',
-			file: 'introduction.m42kup'
+			file: 'introduction.yamd'
 		},
 		{
 			name: '설정 파일',
-			file: 'config-file.m42kup'
+			file: 'config-file.yamd'
 		},
 		{
 			name: '커스텀 템플릿',
-			file: 'templates.m42kup'
+			file: 'templates.yamd'
 		},
 		{
 			name: '꿀팁',
-			file: 'honeytip.m42kup'
+			file: 'honeytip.yamd'
 		},
 	]
 };
